130b9d6ff648c372a4a725a8ec4f3cddabcfac44
kayzhang <kaiwhu@gmail.com>
2016-05-25 16:47:40 +0800

    Translate tabs to spaces.

--- MODE/MODE.m
+++ MODE/MODE.m
@@ -3,91 +3,91 @@ clear;
 close all;
 
 %% Problem Definition
-costFunctionType = 'ZDT3';	% Cost Function Type
+costFunctionType = 'ZDT3';  % Cost Function Type
 [nVar, nObj, nCons, varMin, varMax] = objectiveDescription(costFunctionType);
-varSize = [1 nVar];			% Size of Decision Variables Matrix
+varSize = [1 nVar];         % Size of Decision Variables Matrix
 
 %% Initialize all parameter of the algotithm
-maxIter = 1000;		% Maxinum Number of Iterations
-popSize = 100;		% Population Size
+maxIter = 1000;     % Maxinum Number of Iterations
+popSize = 100;      % Population Size
 
-F = 0.5;			% Scaling Factor
-Cr = 0.2;			% Crossover Rate
+F = 0.5;            % Scaling Factor
+Cr = 0.2;           % Crossover Rate
 
 %% A. Initialization of the Parameter Vector
-parent = zeros(popSize, nVar);	% Parent Population
-mutant = zeros(popSize, nVar);	% Mutant Population
-child  = zeros(popSize, nVar);	% Child Population
+parent = zeros(popSize, nVar);  % Parent Population
+mutant = zeros(popSize, nVar);  % Mutant Population
+child  = zeros(popSize, nVar);  % Child Population
 
 for i = 1:popSize
-	% Initialize
-	for j = 1:nVar
-		parent(i, j) = varMin(j) + rand*(varMax(j) - varMin(j));
-	end
+    % Initialize
+    for j = 1:nVar
+        parent(i, j) = varMin(j) + rand*(varMax(j) - varMin(j));
+    end
 
-	% Evaluate
-	cost(i, :) = evaluate(costFunctionType, parent(i, :));
+    % Evaluate
+    cost(i, :) = evaluate(costFunctionType, parent(i, :));
 end
 
 %% Evolution Process
 for n = 1:maxIter
 
-	for i = 1:popSize
+    for i = 1:popSize
 
-		% B. Mutation with Different Vectors
-		% Generate three mutually exclusive integers randomly chosen
-		% from range [1, popSize], which are also different form the
-		% base vector index i.
-		rev = randperm(popSize);
-		while any(rev(1: 3)==i)
-			rev = randperm(popSize);
-		end
+        % B. Mutation with Different Vectors
+        % Generate three mutually exclusive integers randomly chosen
+        % from range [1, popSize], which are also different form the
+        % base vector index i.
+        rev = randperm(popSize);
+        while any(rev(1: 3)==i)
+            rev = randperm(popSize);
+        end
 
-		% Mutant vector calculation
-		mutant(i, :) = parent(rev(1,1), :)...
-		+ F*(parent(rev(1, 2), :) - parent(rev(1,3), :));
+        % Mutant vector calculation
+        mutant(i, :) = parent(rev(1,1), :)...
+        + F*(parent(rev(1, 2), :) - parent(rev(1,3), :));
 
-		for j = 1:nVar
-			if mutant(i, j) < varMin(j)
-				mutant(i, j) = varMin(j);
-			elseif mutant(i, j) > varMax(j)
-				mutant(i, j) = varMax(j);
-			end
-		end
+        for j = 1:nVar
+            if mutant(i, j) < varMin(j)
+                mutant(i, j) = varMin(j);
+            elseif mutant(i, j) > varMax(j)
+                mutant(i, j) = varMax(j);
+            end
+        end
 
-		% C. Crossover
-		% Binomial Crossover
-		jrand = randi(nVar);
-		for j = 1:nVar
-			if (rand<=Cr) || (j==jrand)
-				child(i, j) = mutant(i, j);
-			else
-				child(i, j) = parent(i, j);
-			end
-		end
+        % C. Crossover
+        % Binomial Crossover
+        jrand = randi(nVar);
+        for j = 1:nVar
+            if (rand<=Cr) || (j==jrand)
+                child(i, j) = mutant(i, j);
+            else
+                child(i, j) = parent(i, j);
+            end
+        end
 
-		% Evaluate the child population
-		childCost(i, :) = evaluate(costFunctionType, child(i, :));
+        % Evaluate the child population
+        childCost(i, :) = evaluate(costFunctionType, child(i, :));
 
-	end
+    end
 
-	% C. Selection
-	for i = 1:popSize
-		if ~(childCost(i, :) > cost(i, :))
-			parent(i, :) = child(i, :);
-			cost(i, :) = childCost(i, :);
-		end
-	end
+    % C. Selection
+    for i = 1:popSize
+        if ~(childCost(i, :) > cost(i, :))
+            parent(i, :) = child(i, :);
+            cost(i, :) = childCost(i, :);
+        end
+    end
 
-	% Plot the result
-	figure(1);
-	plot(cost(:, 1), cost(:, 2), 'r*');
-	xlabel('1^{st} Objective');
-	ylabel('2^{st} Objective');
-	grid on;
-	pause(0.01);
+    % Plot the result
+    figure(1);
+    plot(cost(:, 1), cost(:, 2), 'r*');
+    xlabel('1^{st} Objective');
+    ylabel('2^{st} Objective');
+    grid on;
+    pause(0.01);
 
-	% Show Iteration Information
-	disp(['Iteration ' num2str(n)]);
+    % Show Iteration Information
+    disp(['Iteration ' num2str(n)]);
 
 end
--- MODE/objectiveDescription.m
+++ MODE/objectiveDescription.m
@@ -1,66 +1,66 @@
 function [nVar, nObj, nCons, varMin, varMax] = objectiveDescription(x);
 
-	switch x
-		case 'ZDT1'
-			nVar = 30;				% Number of Decision Variables
-			nObj = 2;
-			nCons = 0;
-			varMin = zeros(1, 30);	% Upper Boundary of Decision Variables
-			varMax = ones(1, 30);	% Lower Boundary of Decision Variables
-		case 'ZDT2'
-			nVar = 30;
-			nObj = 2;
-			nCons = 0;
-			varMin = zeros(1, 30);
-			varMax = ones(1, 30);
-		case 'ZDT3'
-			nVar = 30;
-			nObj = 2;
-			nCons = 0;
-			varMin = zeros(1, 30);
-			varMax = ones(1, 30);
-		case 'ZDT4'
-			nVar = 10;
-			nObj = 2;
-			nCons = 0;
-			varMin = [0, -5*ones(1, 9)];
-			varMax = [1, 5*ones(1, 9)];
+    switch x
+        case 'ZDT1'
+            nVar = 30;              % Number of Decision Variables
+            nObj = 2;
+            nCons = 0;
+            varMin = zeros(1, 30);  % Upper Boundary of Decision Variables
+            varMax = ones(1, 30);   % Lower Boundary of Decision Variables
+        case 'ZDT2'
+            nVar = 30;
+            nObj = 2;
+            nCons = 0;
+            varMin = zeros(1, 30);
+            varMax = ones(1, 30);
+        case 'ZDT3'
+            nVar = 30;
+            nObj = 2;
+            nCons = 0;
+            varMin = zeros(1, 30);
+            varMax = ones(1, 30);
+        case 'ZDT4'
+            nVar = 10;
+            nObj = 2;
+            nCons = 0;
+            varMin = [0, -5*ones(1, 9)];
+            varMax = [1, 5*ones(1, 9)];
         case 'ZDT6'
-			nVar = 10;
-			nObj = 2;
-			nCons = 0;
-			varMin = zeros(1, 10);
-			varMax = ones(1, 10);
-		case 'SCH'
-			nVar = 1;
-			nObj = 2;
-			nCons = 0;
-			varMin = -1000;
-			varMax = 1000;
-		case 'KUR'
-			nVar = 3;
-			nObj = 2;
-			nCons = 0;
-			varMin = -5*ones(1, 3);
-			varMax = 5*ones(1, 3);
-		case 'Test3'
-			nVar = 2;
-			nObj = 2;
-			nCons = 0;
-			varMin = [0 -30];
-			varMax = [1 30];
-		case 'SRN'
-			nVar = 2;
-			nObj = 2;
-			nCons = 2;
-			varMin = [-20 -20];
-			varMax = [20 20];
-		case 'OSY'
-			nVar = 6;
-			nObj = 2;
-			nCons = 6;
-			varMin = [0 0 1 0 1 0];
-			varMax = [10 10 5 6 5 10];
-	end
+            nVar = 10;
+            nObj = 2;
+            nCons = 0;
+            varMin = zeros(1, 10);
+            varMax = ones(1, 10);
+        case 'SCH'
+            nVar = 1;
+            nObj = 2;
+            nCons = 0;
+            varMin = -1000;
+            varMax = 1000;
+        case 'KUR'
+            nVar = 3;
+            nObj = 2;
+            nCons = 0;
+            varMin = -5*ones(1, 3);
+            varMax = 5*ones(1, 3);
+        case 'Test3'
+            nVar = 2;
+            nObj = 2;
+            nCons = 0;
+            varMin = [0 -30];
+            varMax = [1 30];
+        case 'SRN'
+            nVar = 2;
+            nObj = 2;
+            nCons = 2;
+            varMin = [-20 -20];
+            varMax = [20 20];
+        case 'OSY'
+            nVar = 6;
+            nObj = 2;
+            nCons = 6;
+            varMin = [0 0 1 0 1 0];
+            varMax = [10 10 5 6 5 10];
+    end
 
 end
